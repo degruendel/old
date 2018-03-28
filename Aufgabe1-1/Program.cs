0e7d1756edac4038616135a124dbc4c763a9dc0f
degruendel <de.gruendel@gmail.com>
2018-03-28 15:18:07 +0200

    Code version 01

--- Aufgabe1-1/Program.cs
+++ Aufgabe1-1/Program.cs
@@ -4,38 +4,28 @@ namespace Aufgabe01
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             var Num = args[1];
             double d = Convert.ToDouble(Num);
-            double A;
-            double V;
             switch (args[0])
             {
                 case "c":
-                    A = getCubeSurface(d);
-                    V = getCubeVolume(d);
+                    getCubeInfo(d);
                     break;
 
                 case "k":
-                    A = getSphereSurface(d);
-                    V = getSphereVolume(d);
-                    Console.WriteLine("Kugel:  A=" + A + "  |  V=" + V);
+                    getSphereInfo(d);
                     break;
 
                 case "o":
-                    A = getOktaederSurface(d);
-                    V = getOktaederVolume(d);
-                    Console.WriteLine("Oktaeder:  A=" + A + "  |  V=" + V);
+                    getOktaederInfo(d);
                     break;
 
                 default:
                     Console.WriteLine("Bitte wähle c, k, o");
                     break;
             }
-
-
         }
 
 
@@ -44,19 +34,17 @@ namespace Aufgabe01
         {
             double d = Kantenlänge;
             double A = 6 * (d * d);
-            //getCubeInfo(A);
             return A;
         }
         public static double getCubeVolume(double Kantenlänge)
         {
             double d = Kantenlänge;
             double V = d * d * d;
-            //getCubeInfo(V);
             return V;
         }
-        public static void getCubeInfo(double Kantenlänge, double A, double V)
+        public static void getCubeInfo(double Kantenlänge)
         {
-            Console.WriteLine("Würfel:  A=" + A + "  |  V=" + V);
+            Console.WriteLine("Würfel:  A=" + getCubeSurface(Kantenlänge) + "  |  V=" + getCubeVolume(Kantenlänge));
         }
 
         //Kugel Berechnung
@@ -72,19 +60,27 @@ namespace Aufgabe01
             double V = (Math.PI * (d * d * d)) / 6;
             return V;
         }
+        public static void getSphereInfo(double Durchmesser)
+        {
+            Console.WriteLine("Kugel:  A=" + getSphereSurface(Durchmesser) + "  |  V=" + getSphereVolume(Durchmesser));
+        }
 
         //Okateder Berechnung
-        public static double getOktaederSurface(double Kantenlänge)
+        public static double getOktaederSurface(double Durchmesser)
         {
-            double d = Kantenlänge;
+            double d = Durchmesser;
             double A = 2 * Math.Sqrt(3) * (d * d);
             return A;
         }
-        public static double getOktaederVolume(double Kantenlänge)
+        public static double getOktaederVolume(double Durchmesser)
         {
-            double d = Kantenlänge;
+            double d = Durchmesser;
             double V = Math.Sqrt(2) * (d * d * d) / 3;
             return V;
+        }
+        public static void getOktaederInfo(double Durchmesser)
+        {
+            Console.WriteLine("Oktaeder:  A=" + getOktaederSurface(Durchmesser) + "  |  V=" + getOktaederVolume(Durchmesser));
         }
     }
 }
