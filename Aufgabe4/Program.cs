f181284c02d3a472fba61578359b8d1fa34034b9
degruendel <de.gruendel@gmail.com>
2018-05-06 09:14:14 +0200

    aufgabe 4

--- Aufgabe4/Program.cs
+++ Aufgabe4/Program.cs
@@ -5,6 +5,34 @@ using System.Linq;
 namespace Aufgabe4
 {
 
+class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new TreeNode<String>();
+            var root = tree.CreateNode("root");
+            var child1 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child1");
+            root.AppendChild(child1);
+            root.AppendChild(child2);
+            var grand11 = tree.CreateNode("grand11");
+            var grand12 = tree.CreateNode("grand12");
+            var grand13 = tree.CreateNode("grand13");
+            child1.AppendChild(grand11);
+            child1.AppendChild(grand12);
+            child1.AppendChild(grand13);
+            var grand111 = tree.CreateNode("grand111");
+            var grand112 = tree.CreateNode("grand112");
+            grand11.AppendChild(grand111);
+            grand11.AppendChild(grand112);
+            var grand21 = tree.CreateNode("grand21");
+            child2.AppendChild(grand21);
+            child1.RemoveChild(grand12);
+
+            root.PrintTree();
+        }
+    }
+
     public class TreeNode<T>
     {
         public T Data;
@@ -35,34 +63,6 @@ namespace Aufgabe4
             {
                 child.PrintTree(AddTree + "*");
             }
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var tree = new TreeNode<String>();
-            var root = tree.CreateNode("root");
-            var child1 = tree.CreateNode("child1");
-            var child2 = tree.CreateNode("child1");
-            root.AppendChild(child1);
-            root.AppendChild(child2);
-            var grand11 = tree.CreateNode("grand11");
-            var grand12 = tree.CreateNode("grand12");
-            var grand13 = tree.CreateNode("grand13");
-            child1.AppendChild(grand11);
-            child1.AppendChild(grand12);
-            child1.AppendChild(grand13);
-            var grand111 = tree.CreateNode("grand111");
-            var grand112 = tree.CreateNode("grand112");
-            grand11.AppendChild(grand111);
-            grand11.AppendChild(grand112);
-            var grand21 = tree.CreateNode("grand21");
-            child2.AppendChild(grand21);
-            child1.RemoveChild(grand12);
-
-            root.PrintTree();
         }
     }
 }
