6744d06de1f21b7cc6edb7f9f663a08267e19c9b
degruendel <de.gruendel@gmail.com>
2018-05-08 12:41:29 +0200

    Generics wdh

---
+++ GenericsWdh/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Auto<Platzhalter>
+{
+    public double PS;
+    public int Zylinder;
+    public double Hubraum;
+    public Platzhalter Farbe;
+}
+
+namespace GenericsWdh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bestellwesen
+            Auto<string> meinAuto = new Auto<string>();
+
+            meinAuto.Farbe = "grau";
+            meinAuto.PS = 100;
+            meinAuto.Zylinder = 4;
+            meinAuto.Hubraum = 2.8;
+            
+            //Produktion
+            Auto<int> anderesAuto = new Auto<int>();
+            anderesAuto.Farbe = 4711;
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
