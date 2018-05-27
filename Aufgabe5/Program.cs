070ed71112fd8f7c42b950ee9030136a4cf5312a
degruendel <de.gruendel@gmail.com>
2018-05-27 20:14:10 +0200

    aufgbe 5

---
+++ Aufgabe5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgabe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public List<Participants<P>> ParticipantsList = new List<ParticipantsList<P>>();
+
+        public List<Course<C>> CourseList = new List<CourseList<C>>();
+
+        public class Person
+        {
+            public string name;
+            public int age;
+        }
+        public class Teilnehmer : Person
+        {
+            public int matrikelnr;
+            public CourseList<C> Course; 
+        }
+        public class Dozent : Person
+        {
+            public string room;
+            public DateTime availabletime;
+            public CourseList<C> Course; 
+
+            public void AddCourse(CourseList<C> course)
+        {
+            CourseList.Add(course);
+        }
+            public void AddParticipants(ParticipantsList<P> Teilnehmer)
+        {
+            ParticipantsList.Add(Teilnehmer);
+        } 
+        }
+        public class Course
+        {
+            public string title;
+            public DateTime date;
+            public string room;
+
+            public string outputInformationText()
+            {
+                return ;
+            }
+        }
+    }
+}
